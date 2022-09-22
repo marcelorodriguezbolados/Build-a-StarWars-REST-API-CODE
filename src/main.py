71f17f5c63100892b2f3c1b9bd7b93745bdbaeb0
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-09-22 00:41:16 +0000

    error al ingresar planetas

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,People,Planet,Favorite_people,Favorite_planet 
 #from models import Person
 
 app = Flask(__name__)
@@ -32,12 +32,53 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    all_user = User.query.all()
+    serializados = list(map(lambda user: user.serialize(), all_user))
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
 
     return jsonify(response_body), 200
+
+@app.route('/People', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    all_people = list( map(lambda people:people.serialize(), all_people))
+    return jsonify(all_people)
+
+@app.route('/People/<int:people_id>', methods=['GET'])
+def solo_people(people_id):
+    solo = People.query.get(people_id)
+    print(solo)
+    return jsonify(solo.serialize())
+
+
+@app.route('/Planet', methods=['GET'])
+def get_planet():
+    all_planet = Planet.query.all()
+    print(all_planet)
+    all_planet = list( map(lambda planet:planet.serialize(), all_planet))
+    return jsonify(all_planet)
+
+@app.route('/Planet/<int:planet_id>', methods=['GET'])
+def solo_planet(planet_id):
+    planetita = Planet.query.get(planet_id)
+    print(planetita)
+    return jsonify(planetita.serialize())
+
+@app.route('/user/Favorite_people', methods=['GET'])
+def get_people_favorite():
+    people_fav = Favorite_people.query.all()
+    print(people_fav)
+    people_fav = list( map(lambda people_fav:peole_fav.serialize(), people_fav))
+    return jsonify(people_fav)
+
+@app.route('/user/Favorite_planet', methods=['GET'])
+def get_planet_favorite():
+    planet_fav = Favorite_planet.query.all()
+    print(planet_fav)
+    planet_fav = list( map(lambda planet_fav:planet_fav.serialize(), planet_fav))
+    return jsonify(planet_fav)
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
